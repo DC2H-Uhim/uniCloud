71cfd0090c7323185c0b9b14722f1708b860a5ef
Uhim <1392309185@qq.com>
2025-01-28 20:14:08 +0800

    first commit

---
+++ demo/pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="home">
+		<view class="out">
+			<view class="row" v-for="item in listArr" :key="item.id">
+				<image :src="item.picurl" mode=""></image>
+				<view>{{item.title}}</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello',
+				listArr:[]
+			}
+		},
+		onLoad() {
+			uniCloud.callFunction({
+				name:"myCloudFun",
+				data:{
+					num:3
+				}
+			}).then(res=>{
+				this.listArr = res.result
+				console.log(this.listArr)
+			})
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+.out{
+		padding:30rpx;
+		.row{
+			border-bottom:1px solid #ccc;
+			padding:20rpx 0;
+		}
+	}
+</style>
